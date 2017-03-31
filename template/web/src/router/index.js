c04655459305e78877d39f96e2af20f1be2e446d
ZhangLei <priccc@ZhangLeideMBP.lan>
2017-03-31 14:02:16 +0800

    add route mode

--- template/web/src/router/index.js
+++ template/web/src/router/index.js
@@ -5,6 +5,7 @@ import Hello from '@/components/Hello'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 Vue.use(Router){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
